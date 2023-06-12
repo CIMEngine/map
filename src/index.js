5fd3ee783d4681379950446a7a436fd06116d2a8
Artemy <artegoser@gmail.com>
2023-06-12 12:48:40 +0300

    fix: displaying polygons after changing style

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ window.onload = async () => {
 
   let converter = new showdown.Converter();
 
-  map.on("load", async () => {
+  map.on("style.load", async () => {
     map.loadImage(
       "https://cimengine.github.io/map/icons/city.png",
       (error, image) => {
