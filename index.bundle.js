edcf5daa94950eae87ebdb5c6a13bfc27c5e603a
artegoser <artegoser@gmail.com>
2023-10-19 10:38:16 +0000

    Deploying to gh-pages from @ CIMEngine/map@6439516bff2be8c7debc83e0da6adba3f89f278e 🚀

--- index.bundle.js
+++ index.bundle.js
@@ -513,7 +513,7 @@
       console.log(
         `%c ${info} `,
         "color:white; background-color: #78d6fa; border-radius:10px;",
-        ...str
+        ...str,
       );
     }
     window.onload = async () => {
@@ -527,7 +527,7 @@
       let mapDataFromId = (
         await (
           await fetch(
-            `https://raw.githubusercontent.com/CIMEngine/MapList/main/index.json`
+            `https://raw.githubusercontent.com/CIMEngine/MapList/main/index.json`,
           )
         ).json()
       )[mapId];
@@ -563,7 +563,7 @@
             },
           ],
         }),
-        "top-left"
+        "top-left",
       );
 
       map.addControl(new ZoomControl(), "top-right");
@@ -577,7 +577,7 @@
           Crosshair: true,
           PrintableArea: true,
         }),
-        "top-right"
+        "top-right",
       );
 
       if (debug) {
@@ -592,7 +592,7 @@
           (error, image) => {
             if (error) throw error;
             map.addImage("city", image);
-          }
+          },
         );
 
         map.loadImage(
@@ -601,7 +601,7 @@
             if (error) throw error;
             map.addImage("capital-city", image);
             map.addImage("capital", image);
-          }
+          },
         );
 
         map.loadImage(
@@ -609,7 +609,7 @@
           (error, image) => {
             if (error) throw error;
             map.addImage("landmark-0", image);
-          }
+          },
         );
 
         let lasticocords;
@@ -627,16 +627,6 @@
         });
 
         map.addLayer({
-          id: "map-data-fill",
-          type: "fill",
-          source: "map-data",
-          paint: {
-            "fill-color": ["get", "fill"],
-            "fill-opacity": ["coalesce", ["get", "fill-opacity"], 0.3],
-          },
-        });
-
-        map.addLayer({
           id: "map-data-fill-outline",
           type: "line",
           source: "map-data",
@@ -644,6 +634,16 @@
             "line-color": ["coalesce", ["get", "stroke"], "#0c7700"],
             "line-width": 2,
             "line-opacity": 0.8,
+          },
+        });
+
+        map.addLayer({
+          id: "map-data-fill",
+          type: "fill",
+          source: "map-data",
+          paint: {
+            "fill-color": ["get", "fill"],
+            "fill-opacity": ["coalesce", ["get", "fill-opacity"], 0.3],
           },
         });
 
@@ -683,35 +683,31 @@
               .setLngLat(coordinates)
               .setHTML(
                 `
-                ${
-                  feature?.properties?.amount
-                    ? `<div class="row glass" style="color: "white";"><div class="col">Population - ${feature.properties.amount} people.</div></div>`
-                    : ""
-                }
+                ${feature?.properties?.amount
+              ? `<div class="row glass" style="color: "white";"><div class="col">Population - ${feature.properties.amount} people.</div></div>`
+              : ""
+            }
                 <div class="row" style="padding: 5px;">
-                  ${
-                    feature?.properties?.img
-                      ? `<div class="col-md-12 col-sm-12" style="padding: 0px;"><img class="w-100 about-img" src="${feature.properties.img}" alt="${feature.properties.name} img"></div>`
-                      : ""
-                  }
+                  ${feature?.properties?.img
+              ? `<div class="col-md-12 col-sm-12" style="padding: 0px;"><img class="w-100 about-img" src="${feature.properties.img}" alt="${feature.properties.name} img"></div>`
+              : ""
+            }
                   <div class="col-md-12 col-sm-12 text-center glass mb-2">
                     <h5 className="card-title">${feature.properties.name}
-                      ${
-                        feature.properties.name_ru
-                          ? ` - ${feature.properties.name_ru}`
-                          : ""
-                      }
+                      ${feature.properties.name_ru
+              ? ` - ${feature.properties.name_ru}`
+              : ""
+            }
                     </h5>
                   </div>
-                  ${
-                    feature.properties.description
-                      ? `<div class="col-md-12 col-sm-12 text-center glass"><div>${converter.makeHtml(
-                          feature.properties.description
-                        )}</div></div>`
-                      : ""
-                  }
+                  ${feature.properties.description
+              ? `<div class="col-md-12 col-sm-12 text-center glass"><div>${converter.makeHtml(
+                feature.properties.description,
+              )}</div></div>`
+              : ""
+            }
                 </div>
-                `
+                `,
               )
               .addTo(map);
           } else if (
@@ -730,9 +726,8 @@
                       `
                     <div class="row" style="padding: 5px;">
                             <div class="col-12 col-sm-12" style="padding: 0px;">
-                                    <img class="w-100 about-img" src="${
-                                      country.img
-                                    }">
+                                    <img class="w-100 about-img" src="${country.img
+                  }">
                             </div>
                             <div class="col-12 text-center glass">
                                     <h5>
@@ -741,32 +736,30 @@
                             </div>
                             <div class="col-12 text-center glass"> 
                                   ${JSON.parse(
-                                    feature.properties.tags || "[]"
-                                  ).join(", ")}
+                    feature.properties.tags || "[]",
+                  ).join(", ")}
                             </div>
                             <div class="col-12 text-center glass"> 
                                 Founding date: ${country.date}
                             </div>
                             <div class="col-md-12 col-sm-12 text-center glass">
-                              ${
-                                country.description
-                                  ? `<div>${converter.makeHtml(
-                                      country.description
-                                    )}</div>`
-                                  : ""
-                              }
+                              ${country.description
+                    ? `<div>${converter.makeHtml(
+                      country.description,
+                    )}</div>`
+                    : ""
+                  }
                             </div>
                             <div class="col-12 text-center glass"> 
                                 Area: ${feature.properties.area} km²
                             </div>
                             <div class="col-12 text-center mt-2">
-                              ${
-                                country.about
-                                  ? `<a href="${country.about}" class="about">About</a>`
-                                  : ""
-                              }
+                              ${country.about
+                    ? `<a href="${country.about}" class="about">About</a>`
+                    : ""
+                  }
                             </div>
-                    </div>`
+                    </div>`,
                     )
                     .addTo(map);
             }, 1);
